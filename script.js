d635c9b8697f22a90e6fc935eca5396df67370b0
Danielle Teodoro <danielle.caldeira@aluno.ifsp.edu.br>
2026-05-11 15:01:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+
+// fetch() 'buscar um arquivo'
 fetch("header.html")
+
+    // transforma o conteúdo em texto HTML
     .then(resposta => resposta.text())
+
+    // coloca o conteúdo dentro da div
     .then(dados => {
         document.getElementById("header").innerHTML = dados;
     });
+
+    // posteriormente o navegador transforma <div id="header"></div>
+    // EM
+    // <header>...</header>
+    // AUTOMATICAMENTE
+
 
 fetch("footer.html")
     .then(resposta => resposta.text())
