af4e2c000058f163d81a7f55a0b6beb2e59f3804
Danielle Teodoro <danielle.caldeira@aluno.ifsp.edu.br>
2026-05-23 11:21:30 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -21,3 +21,87 @@ fetch("footer.html")
     .then(dados => {
         document.getElementById("footer").innerHTML = dados;
     });
+
+    // =====================================
+// HEADER
+// carrega o header.html automaticamente
+// =====================================
+
+fetch("header.html")
+    .then(resposta => resposta.text())
+    .then(dados => {
+        document.getElementById("header").innerHTML = dados;
+    });
+
+// Função executada ao clicar no botão ☰
+function abrirMenu(){
+
+    // procura o elemento com id="menu"
+    const menu = document.getElementById("menu");
+
+    // adiciona ou remove a classe "ativo"
+    // se estiver fechado, abre
+    // se estiver aberto, fecha
+    menu.classList.toggle("ativo");
+}
+
+
+// =====================================
+// FOOTER
+// carrega o footer.html automaticamente
+// =====================================
+
+fetch("footer.html")
+    .then(resposta => resposta.text())
+    .then(dados => {
+        document.getElementById("footer").innerHTML = dados;
+    });
+
+
+
+
+// =====================================
+// CARROSSEL DE IMAGENS
+// =====================================
+
+// seleciona todas as imagens com a classe "slide"
+let slides = document.querySelectorAll(".slide");
+
+// controla qual imagem está aparecendo
+let indiceAtual = 0;
+
+
+
+// função que troca as imagens
+function trocarSlide(){
+
+    // remove a classe "ativo" de todas as imagens
+    slides.forEach(function(slide){
+
+        slide.classList.remove("ativo");
+
+    });
+
+
+    // passa para a próxima imagem
+    indiceAtual++;
+
+
+    // quando chegar na última imagem,
+    // volta para a primeira
+    if(indiceAtual >= slides.length){
+
+        indiceAtual = 0;
+
+    }
+
+
+    // mostra a imagem atual
+    slides[indiceAtual].classList.add("ativo");
+
+}
+
+
+
+// troca automaticamente a cada 3 segundos
+setInterval(trocarSlide, 3000);
